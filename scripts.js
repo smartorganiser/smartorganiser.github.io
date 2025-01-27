9fccb2a7ffa878125aba3d54904bf84d474ab8c7
smartdeskorganiser <alisamedov18@gmail.com>
2025-01-27 23:21:16 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,4 @@
-// Animasyonları tetiklemek için scroll dinleyicisi
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('.section');
-
-    window.addEventListener('scroll', () => {
-        sections.forEach((section) => {
-            if (isInViewport(section)) {
-                section.classList.add('active');
-            }
-        });
-    });
-
-    // Scroll ile element görünürse aktif et
-    function isInViewport(el) {
-        const rect = el.getBoundingClientRect();
-        return rect.top >= 0 && rect.bottom <= window.innerHeight;
-    }
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Mesajınız başarıyla gönderildi!");
 });
